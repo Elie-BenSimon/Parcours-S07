8e54b16e4343aa4f90552f5914f099991f1f8cbe
Elie-BenSimon <eliebensimon_2@hotmail.com>
2022-02-03 09:54:37 +0100

    retrieving review by videogame

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,44 +1,117 @@
 const app = {
-    init: function() {
+    // URL de l'API (sans le endpoint)
+    apiUrl: 'http://localhost:8080',
+
+    videoGameId: null,
+
+    init: function () {
         console.log('app.init()');
 
         // On appelle la méthode s'occupant d'ajouter les EventListener sur les éléments déjà dans le DOM
         app.addAllEventListeners();
 
         // On appelle la méthode s'occupant de charger tous les jeux vidéo
-        app.loadVideoGames();
-    },
-    addAllEventListeners: function() {
-        // On récupère l'élément <select> des jeux vidéo
-        
-        // On ajoute l'écouteur pour l'event "change", et on l'attache à la méthode app.handleVideogameSelected
+        //app.loadVideoGames();
 
+        //app.loadReviewFromApi();
+    },
+
+
+    // ####################################################################
+    //                              EVENTS
+    // ####################################################################
+
+    addAllEventListeners: function () {
+        // On récupère l'élément <select> des jeux vidéo
+        selectVideogameElement = document.querySelector('#videogameId');
+        // On ajoute l'écouteur pour l'event "change", et on l'attache à la méthode app.handleVideogameSelected
+        selectVideogameElement.addEventListener('change', app.handleVideogameSelected)
         // On récupère le bouton pour ajouter un jeu vidéo
         const addVideogameButtonElement = document.getElementById('btnAddVideogame');
         // On ajoute l'écouteur pour l'event "click"
         addVideogameButtonElement.addEventListener('click', app.handleClickToAddVideogame);
-        
+
         // TODO
     },
-    handleVideogameSelected: function(evt) {
-        // Récupérer la valeur du <select> (id du videogame)
 
+    handleVideogameSelected: function (evt) {
+        // Récupérer la valeur du <select> (id du videogame)
+        videoGameId = evt.target.selectedIndex;
         // Vider le contenu de div#review
 
         // charger les données pour ce videogame
-            // Dupliquer la template #reviewTemplate et personnaliser son contenu avec les données
+        app.loadReviewFromApi();
+        //console.log(videogameDomElement);
+        // Dupliquer la template #reviewTemplate et personnaliser son contenu avec les données
 
-            // Ajouter dans le DOM
+        // Ajouter dans le DOM
     },
-    handleClickToAddVideogame: function(evt) {
+
+    handleClickToAddVideogame: function (evt) {
         // https://getbootstrap.com/docs/4.4/components/modal/#modalshow
         // jQuery obligatoire ici
         $('#addVideogameModal').modal('show');
     },
-    loadVideoGames: function() {
+
+
+    // ####################################################################
+    //                              AJAX
+    // ####################################################################
+
+    loadReviewFromApi: function () {
+        // On prépare la configuration de la requête HTTP
+        let config = {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache'
+        };
+
+        // 1 - Attaquer l'API
+        fetch(app.apiUrl + '/reviews' , config)
+            .then(function (apiResponse) {
+                return apiResponse.json();
+            })
+            .then(app.addReviewFromApi);
+    },
+
+    addReviewFromApi: function (reviewFromApi) {
+        console.log(reviewFromApi);
+        for (const review of reviewFromApi) {
+            if (review.videogame_id === videoGameId) {
+                console.log(review);
+            }
+        }
+    },
+
+    loadSingleVideoGame: function (id) {
         // Charger toutes les données des videogames
-            // Ajouter une balise <option> par videogame
-    }
+        // Ajouter une balise <option> par videogame
+
+        // On prépare la configuration de la requête HTTP
+        let config = {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache'
+        };
+
+        // 1 - Attaquer l'API
+        fetch(app.apiUrl + '/videogames/' + id, config)
+            .then(function (apiResponse) {
+                return apiResponse.json();
+            })
+            .then(function(videogame) {
+                console.log(videogame);
+            });
+            return videogame;
+    },
+
+    addVideogameFromApi: function (videogame) {
+    },
+
+
+    // ####################################################################
+    //                              DOM
+    // ####################################################################
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
