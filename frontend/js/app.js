45d03c5dbcd03929a5a3b3f0641e162217dbb5d8
Elie-BenSimon <eliebensimon_2@hotmail.com>
2022-02-03 12:03:46 +0100

    the end!

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -20,66 +20,93 @@ const app = {
     //                              EVENTS
     // ####################################################################
 
+    /**
+     * Method to add every event listener to dom elements
+     */
     addAllEventListeners: function () {
         // event listener on "change", with app.handleVideogameSelected callback function
         app.selectVideogameElement.addEventListener('change', app.handleVideogameSelected)
+
         // add a videogame button
         const addVideogameButtonElement = document.getElementById('btnAddVideogame');
         // eventlistener on click
         addVideogameButtonElement.addEventListener('click', app.handleClickToAddVideogame);
+
         // add a videogame form selection
         addVideogameFormElement = document.querySelector('#addVideogameForm');
         // eventistener on form submission
         addVideogameFormElement.addEventListener('submit', app.handleFormSubmit);
     },
 
+    /**
+     * Handle the form to add a videogame in db and in select list
+     * 
+     * @param {event} event 
+     */
     handleFormSubmit: function (event) {
+        // we prevent normal submission first
         event.preventDefault();
+
+        // Retrieving inputs value
         const newVideogameFormElement = event.currentTarget;
         const videogameTitleElement = newVideogameFormElement.querySelector('#inputName');
         const newVideogameTitle = videogameTitleElement.value;
-
         const videogameEditorElement = newVideogameFormElement.querySelector('#inputEditor');
         const newVideogameEditor = videogameEditorElement.value;
 
-        const data = {
-            "name": newVideogameTitle,
-            "editor": newVideogameEditor,
+        if (newVideogameTitle === "") {
+            document.querySelector('#nameError').textContent = "veuillez remplir le nom du jeu";
         }
+        else if (newVideogameEditor === "") {
+            document.querySelector('#nameError').textContent = ""
+            document.querySelector('#editorError').textContent = "veuillez remplir l'éditeur du jeu";
+        }
+        else {
+            // data setup before send
+            const data = {
+                "name": newVideogameTitle,
+                "editor": newVideogameEditor,
+            }
 
-        console.log(data);
+            // header preparation
+            const httpHeaders = new Headers();
+            httpHeaders.append("Content-Type", "application/json");
 
-        const httpHeaders = new Headers();
-        httpHeaders.append("Content-Type", "application/json");
+            // configuration of query
+            let config = {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                headers: httpHeaders,
+                body: JSON.stringify(data)
+            };
 
-        let config = {
-            method: 'POST',
-            mode: 'cors',
-            cache: 'no-cache',
-            headers: httpHeaders,
-            body: JSON.stringify(data)
-        };
-
-        fetch(app.apiUrl + "/videogames", config)
-            .then(
-                function (response) {
-                    if (response.status == 201) {
-                        $('#addVideogameModal').modal('hide');
-                        alert('jeu vidéo ajouté');
-                        return response.json();
+            fetch(app.apiUrl + "/videogames", config)
+                .then(
+                    function (response) {
+                        if (response.status == 201) {
+                            $('#addVideogameModal').modal('hide');
+                            alert('jeu vidéo ajouté');
+                            return response.json();
+                        }
+                        else {
+                            alert('le jeu n\'a pas été ajouté... déso bro');
+                        }
                     }
-                    else {
-                        alert('le jeu n\'a pas été ajouté, veuillez renseigner les champs correctement');
+                )
+                .then(
+                    function (newTaskObject) {
+                        app.addVideoGameInList(newTaskObject);
                     }
-                }
-            )
-            .then(
-                function (newTaskObject) {
-                    app.addVideoGameInList(newTaskObject);
-                }
-            )
+                )
+        }
     },
 
+    /**
+     *  Handle the selection of a videogame in list
+     * 
+     * @param {event} evt 
+     */
     handleVideogameSelected: function (evt) {
         // Récupérer la valeur du <select> (id du videogame)
         videoGameId = evt.target.selectedIndex;
@@ -89,6 +116,11 @@ const app = {
         app.loadReviewFromApi(videoGameId);
     },
 
+    /**
+     * Handle the click on the "ajouter" button to add a new videogame
+     * 
+     * @param {event} evt 
+     */
     handleClickToAddVideogame: function (evt) {
         // https://getbootstrap.com/docs/4.4/components/modal/#modalshow
         // jQuery obligatoire ici
@@ -101,14 +133,14 @@ const app = {
     // ####################################################################
 
     loadReviewFromApi: function (id) {
-        // On prépare la configuration de la requête HTTP
+        // request configuration
         let config = {
             method: 'GET',
             mode: 'cors',
             cache: 'no-cache'
         };
 
-        // 1 - Attaquer l'API
+        // ajax query to api
         fetch(app.apiUrl + '/videogames/' + id + '/reviews', config)
             .then(function (apiResponse) {
                 return apiResponse.json();
@@ -116,34 +148,15 @@ const app = {
             .then(app.addReviewFromApi);
     },
 
-    loadSingleVideoGame: function (id) {
-        // On prépare la configuration de la requête HTTP
-        let config = {
-            method: 'GET',
-            mode: 'cors',
-            cache: 'no-cache'
-        };
-
-        // 1 - Attaquer l'API
-        fetch(app.apiUrl + '/videogames/' + id, config)
-            .then(function (apiResponse) {
-                return apiResponse.json();
-            })
-            .then(function (videogame) {
-                console.log(videogame);
-            });
-        return videogame;
-    },
-
     loadVideoGames: function () {
-        // On prépare la configuration de la requête HTTP
+        // request configuration
         let config = {
             method: 'GET',
             mode: 'cors',
             cache: 'no-cache'
         };
 
-        // 1 - Attaquer l'API
+        // ajax query to api
         fetch(app.apiUrl + '/videogames', config)
             .then(function (apiResponse) {
                 return apiResponse.json();
@@ -159,7 +172,7 @@ const app = {
     addReviewFromApi: function (reviewFromApi) {
         // Selection of div review's container
         divReviewElement = document.querySelector('#review');
-        
+
         for (const review of reviewFromApi) {
             // Template selection and cloning
             const newReviewTemplate = document.querySelector('#reviewTemplate');
@@ -174,7 +187,7 @@ const app = {
             newReviewElement.querySelector('.reviewGameplay').textContent = review.gameplay_note;
             newReviewElement.querySelector('.reviewScenario').textContent = review.scenario_note;
             newReviewElement.querySelector('.reviewLifetime').textContent = review.lifetime_note;
-            
+
             newReviewElement.querySelector('.reviewVideogame').textContent = review.videogame.name;
             newReviewElement.querySelector('.reviewEditor').textContent = review.videogame.editor;
             newReviewElement.querySelector('.reviewPlatform').textContent = review.platform.name;
